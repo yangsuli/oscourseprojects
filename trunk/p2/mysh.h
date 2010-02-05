6f6b1b562a5415a7368dad6f943bf50b20898c2a
yangsuli <yangsuli@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-02-05 05:14:19 +0000

    support for '+'. Now has bugs....

--- trunk/p2/mysh.h
+++ trunk/p2/mysh.h
@@ -1,8 +1,11 @@
 #define MAX_LINE_LENGTH 512
 #define MAX_ARGUMENTS 512
+#define MAX_COMMANDS 512
 
 void read_one_line(char * buf, int n, FILE * input);
 
 bool is_eol(char x);
 
 void initialize_buf_with_eol(char *buf, int size);
+
+void run_parallel_commands(char* parallel_cmd);
