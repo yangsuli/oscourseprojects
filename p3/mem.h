d65c7035eb7469b57663ca6d0bdb67bf37064273
yangsuli <yangsuli@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-02-21 03:09:33 +0000

    add some debug mode, updated mem.h

--- p3/mem.h
+++ p3/mem.h
@@ -1,10 +1,11 @@
 #ifndef __mem_h__
 #define __mem_h__
 
-#define E_NO_SPACE (1)
-#define E_CORRUPT_FREESPACE (2)
+#define E_NO_SPACE            (1)
+#define E_CORRUPT_FREESPACE   (2)
 #define E_PADDING_OVERWRITTEN (3)
-#define E_BAD_ARGS (4)
+#define E_BAD_ARGS            (4)
+#define E_BAD_POINTER         (5)
 
 extern int m_error;
 
