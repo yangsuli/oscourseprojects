cc046e17a047110f475dae95dc58a2681929cd06
yangsuli <yangsuli@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-03-21 04:36:58 +0000

    swith between spinlock and pthread_lock steamlessly

--- p4/counter.c
+++ p4/counter.c
@@ -1,29 +1,30 @@
 #include "counter.h"
-#include <pthread.h>
+#include "lock_type.h"
 
 void Counter_Init(counter_t *c, int value){
 	c -> count = value; // no need to protect this initialization
-	c -> counter_lock = PTHREAD_MUTEX_INITIALIZER;
+	//c -> counter_lock = LOCK_INITIALIZER;
+	LOCK_INIT(&c->counter_lock,NULL);
 }
 
 int Counter_GetValue(counter_t *c){
 	int n_val;
 	//lock needed here since if one thread is updating this value, we would like to wait until it's done and then get the new value
 	//also, if the write operation  cannot be done within one instruction, then lock become necessary here. So, also for portability
-	pthread_mutex_lock(&c->counter_lock);
+	LOCK(&c->counter_lock);
 	n_val = c -> count;
-	pthread_mutex_unlock(&c->counter_lock);
+	UNLOCK(&c->counter_lock);
 	return n_val;
 }
 
 void Counter_Increment(counter_t *c){
-	pthread_mutex_lock(&c->counter_lock);
+	LOCK(&c->counter_lock);
 	c -> count ++;
-	pthread_mutex_unlock(&c->counter_lock);
+	UNLOCK(&c->counter_lock);
 }
 
 void Counter_Decrement(counter_t *c){
-	pthread_mutex_lock(&c->counter_lock);
+	LOCK(&c->counter_lock);
 	c -> count --;
-	pthread_mutex_unlock(&c->counter_lock);
+	UNLOCK(&c->counter_lock);
 }
---
+++ p4/lock_type.h
@@ -0,0 +1,15 @@
+#include <pthread.h>
+
+#ifdef SPINLOCK
+#define LOCK_TYPE spinlock_t
+#define LOCK_INITIALIZER SPINLOCK_INITIALIZER
+#define LOCK_INIT(lock_p,attr_p) spinlock_init(lock_p,attr_p)
+#define LOCK(lock_p) spinlock_acquire(lock_p)
+#define UNLOCK(lock_p) spinlock_release(lock_p)
+#elif defined PTHREAD_LOCK
+#define LOCK_TYPE pthread_mutex_t
+#define LOCK_INITIALIZER PTHREAD_MUTEX_INITIALIZER
+#define LOCK_INIT(lock_p,attr_p) pthread_mutex_init(lock_p,attr_p)
+#define LOCK(lock_p) pthread_mutex_lock(lock_p)
+#define UNLOCK(lock_p) pthread_mutex_unlock(lock_p)
+#endif
