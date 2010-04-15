4839b2f73c2a58a7ac0db08ca97846aca2819843
seal.david <seal.david@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-04-15 02:58:58 +0000

    added a couple of missing posix wrapper functions to cs537.c

--- p5/cs537.h
+++ p5/cs537.h
@@ -122,12 +122,17 @@ int Open_clientfd(char *hostname, int port);
 int Open_listenfd(int port); 
 
 /* Wrappers for Posix threads */
-void Pthread_mutex_lock(pthread_mutex_t *m);
-void Pthread_mutex_unlock(pthread_mutex_t *m);
 void Pthread_create(pthread_t *thread, const pthread_attr_t *attr,   
            void *(*start_routine)(void*), void *arg);
-void Pthread_join(pthread_t thread, void **value_ptr);
-int Pthread_cond_signal(pthread_cond_t *cond);
-int Pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
 
+void Pthread_mutex_init(pthread_mutex_t *mutex,
+              const pthread_mutexattr_t *attr);
+void Pthread_mutex_lock(pthread_mutex_t *m);
+void Pthread_mutex_unlock(pthread_mutex_t *m);
+
+void Pthread_cond_init(pthread_cond_t *cond, const pthread_condattr_t *attr);
+void Pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex);
+void Pthread_cond_signal(pthread_cond_t *cond);
+
+void Pthread_join(pthread_t thread, void **value_ptr);
 #endif /* __CSAPP_H__ */
