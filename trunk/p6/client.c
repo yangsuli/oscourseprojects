3fb71895332e78047d97dfcd3363bca4d08d4683
seal.david <seal.david@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-05-03 05:05:03 +0000

    minor changes -- need to switch computers to use -fpic

--- trunk/p6/client.c
+++ trunk/p6/client.c
@@ -28,4 +28,13 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-
+// need to implement these public routines
+/*
+int MFS_Init(char *hostname, int port);
+int MFS_Lookup(int pinum, char *name);
+int MFS_Stat(int inum, MFS_Stat_t *m);
+int MFS_Write(int inum, char *buffer, int block);
+int MFS_Read(int inum, char *buffer, int block);
+int MFS_Creat(int pinum, int type, char *name);
+int MFS_Unlink(int pinum, char *name);
+*/
