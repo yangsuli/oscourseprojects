c6161b3aa876ef2d090a201393425b358a0c8d20
seal.david <seal.david@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-05-03 18:34:26 +0000

    more grunt work on client.c -- still need to put together the send message routine

--- p6/mssg.h
+++ p6/mssg.h
@@ -10,12 +10,12 @@
 typedef struct __params {
     int func_num;          // function number being called 0 <= func_num <= 6
     char name[BUFFER_SIZE];
-    char buffer[BUFFER_SIZE];
-    void * blocks;
+    char buffer[BUFFER_SIZE]; // generic data is put here ...
+    int block;              // block number
+    int blocks;             // number of blocks allocated to inum
     int pinum;              // parent inode number
     int inum;               // inode number
     int size;
-    int block;              // block number
     int status;             // return status for server
     int type;               // either MFS_DIRECTORY or MFS_REGULAR_FILE
 } Params;
@@ -52,11 +52,11 @@ MFS_INIT (func_num == 0) :
 MFS_Lookup (func_num == 1): 
     client send (int pinum, char *name)
     server: (int inum)
-        if invalid pinum, '-3' if name does exist.
+        if invalid pinum TODO ??? , '-3' if name does exist.
 
 MFS_Stat (func_num == 2): 
     client send (int inum)
-    server send (int status, int type, int size, void * blocks)
+    server send (int status, int type, int size, int blocks)
         '0'  'type' 'size' 'blocks' on sucess (4 strings in total, and ordering is important)
         status == -1 on failure or inum doesn't exist
 
