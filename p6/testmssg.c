99e7a340b2470ebd7ce108defce57bbfa8aadbdf
seal.david <seal.david@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-05-03 03:25:23 +0000

    more changes to mssg and testmssg -- started working on client.c

--- p6/testmssg.c
+++ p6/testmssg.c
@@ -9,7 +9,6 @@
 int test0() {
 
     char buffer1[BUFFER_SIZE];    
-//  char buffer2[BUFFER_SIZE];    
 
     int msg_len1[NUM_MESSAGES];
     int msg_len2[NUM_MESSAGES];
@@ -17,33 +16,34 @@ int test0() {
     void * data1[NUM_MESSAGES];
     void * data2[NUM_MESSAGES];
 
-    int i;
-    for(i = 0; i < NUM_MESSAGES; i++ ) {
-        msg_len1[i] = 0;
-        msg_len2[i] = 0;
-        data1[i] = malloc( BUFFER_SIZE );
-        data2[i] = malloc( BUFFER_SIZE );
-    }
-
+    Params params;
+    InitData( msg_len1, data1, &params );
+    InitData( msg_len2, data2, &params );
     int type1 = -1;
     int type2 = -1;
+
+    int fun_num1 = -1;
+    int fun_num2 = -1;
 
     // create a message
     type1 = 0;
     int x = 10;
     int * p = &x;
-    char word1[] = "hi mom!";
     data1[0] = (void *) p;
+
+    char word1[] = "hi mom!";
     strncpy( (char*) data1[1], word1, BUFFER_SIZE);
     msg_len1[0] = sizeof(int);
     msg_len1[1] = (int) (strlen( (char*) data1[1] ) + 1);
 
-    CreateMessage( type1, msg_len1, data1, buffer1);
+    fun_num1 = 3;
+    CreateMessage( type1, fun_num1, msg_len1, data1, buffer1);
 
     // read the message
-    ReadMessage( &type2, msg_len2, data2, buffer1);
+    ReadMessage( &type2, &fun_num2, msg_len2, data2, buffer1);
 
     // check if everything is the same
+    int i;
     for( i = 0; i < NUM_MESSAGES; i++) {
         assert( msg_len1[i] == msg_len2[i] );
     }
@@ -53,6 +53,7 @@ int test0() {
         assert( msg_len1[i] == 0 );
     }
 
+    printf("   fun_num1 = %d, fun_num2 = %d\n", fun_num1, fun_num2);
     printf("   data1[0] = %d;", * ((int *) data1[0] ) );
     printf("   data2[0] = %d\n", * ((int *) data2[0] ) );
     printf("   data1[1] = %s;", (char*) data1[1] );
@@ -68,11 +69,7 @@ int test0() {
 // check message type 1
 int test1() {
 
-// THIS IS CURRENTLY A DUPLICATE OF TEST0 -- CHANGE THIS TO TEST MESSAGE TYPE
-// 1
-/*
     char buffer1[BUFFER_SIZE];    
-    char buffer2[BUFFER_SIZE];    
 
     int msg_len1[NUM_MESSAGES];
     int msg_len2[NUM_MESSAGES];
@@ -86,6 +83,8 @@ int test1() {
         msg_len2[i] = 0;
         data1[i] = malloc( BUFFER_SIZE );
         data2[i] = malloc( BUFFER_SIZE );
+        assert( data1[i] != NULL );
+        assert( data2[i] != NULL );
     }
 
     int type1 = -1;
@@ -96,15 +95,19 @@ int test1() {
     int x = 10;
     int * p = &x;
     char word1[] = "hi mom!";
+    char word2[] = "what!?";
     data1[0] = (void *) p;
     strncpy( (char*) data1[1], word1, BUFFER_SIZE);
+    strncpy( (char*) data1[2], word2, BUFFER_SIZE);
     msg_len1[0] = sizeof(int);
     msg_len1[1] = (int) (strlen( (char*) data1[1] ) + 1);
+    msg_len1[2] = (int) (strlen( (char*) data1[2] ) + 1);
 
-    CreateMessage( type1, msg_len1, data1, buffer1);
+    CreateMessage( type1, 3, msg_len1, data1, buffer1);
 
     // read the message
-    ReadMessage( &type2, msg_len2, data2, buffer1);
+    int fun_num2 = -1;
+    ReadMessage( &type2, &fun_num2, msg_len2, data2, buffer1);
 
     // check if everything is the same
     for( i = 0; i < NUM_MESSAGES; i++) {
@@ -116,11 +119,11 @@ int test1() {
     printf("   data2[0] = %d\n", * ((int *) data2[0] ) );
     printf("   data1[1] = %s;", (char*) data1[1] );
     printf("   data2[1] = %s\n", (char*) data2[1] );
+    printf("   data1[2] = %s;", (char*)  data1[2] );
+    printf("   data2[2] = %s\n", (char*) data2[2] );
 
     printf("  does everything match up with each other?\n");
-    pause(1.0);
-
-*/
+    sleep(1.0);
 
     return 0;
 
