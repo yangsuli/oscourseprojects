29d966b3b45c61dec238e8b28c628942d3f212cc
yangsuli <yangsuli@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-09-28 13:48:31 +0000

    measure seek time... seems like I have an overflow error

--- trunk/hw1/disk.c
+++ trunk/hw1/disk.c
@@ -5,25 +5,39 @@
 #include <sched.h>
 #include <stdlib.h>
 
-#define BUFSIZE 1024*1024*100 //100M
 #define SECTOR_SIZE 512
 #define SECTORS_PER_TRACK 63
 #define TRACK_SIZE SECTORS_PER_TRACK*SECTOR_SIZE
 #define HEADS 255
 #define CYLINDER_SIZE TRACK_SIZE*HEADS
+#define CYLINDERS 60801
 //TODO UNITS??
 
-char buf[SECTOR_SIZE];
+
+#define BUFSIZE SECTOR_SIZE 
+#define SEEK_TIME 100
+#define SEEK_SIZE CYLINDER_SIZE*CYLINDERS/(SEEK_TIME + 10)
+
+
+/*
+   potential problems:
+   1. hard disk drive cache
+   2. other overhead (sys call etc.)
+   3. bus contention/ other transmission time
+   4. acceleartion, costing, settling simplified
+ */
+
 
 int main(){
 	struct sched_param param;
 	int fd;
-	double time1, time2;
-	double seek_time;
+	double time1, time2, time3, time4;
+	double seek_time, total_time;
+	double time[SEEK_TIME];
 	cpu_set_t run_set;
-	//void **buf_ptr_ptr;
-	void *buf_ptr = valloc(1024);
+	void *buf_ptr = valloc(SECTOR_SIZE);
 	assert(buf_ptr != NULL);
+	int i;
 
 	CPU_ZERO(&run_set);
 	CPU_SET(0,&run_set);
@@ -32,21 +46,37 @@ int main(){
 	param.sched_priority = sched_get_priority_max(SCHED_FIFO);
 	assert(sched_setscheduler(0,SCHED_FIFO,&param) == 0);
 	
-	//assert( posix_memalign(buf_ptr_ptr,SECTOR_SIZE*2,SECTOR_SIZE*2) == 0);
-	
+	get_time_offset();
 	assert((fd = open("/dev/sda", O_RDONLY|O_DIRECT)) >= 0);
-        get_time_offset();
+	assert(lseek64(fd, 0, SEEK_SET) == 0);
+	assert(read(fd,buf_ptr,SECTOR_SIZE) == SECTOR_SIZE);
 
-	fprintf(stderr,"%d\n",lseek(fd, 0, SEEK_SET));
+
+	/*fprintf(stderr,"%d\n",lseek(fd, 0, SEEK_SET));
 	time1 = get_time();
 	fprintf(stderr,"%d\n",lseek(fd, 4096*1000, SEEK_CUR));
 	time2 = get_time();
-	//assert(read(fd,buf,1024*1024*100) == 1024*1024*100);
-	fprintf(stderr,"%d %d %d\n", read(fd,buf_ptr,SECTOR_SIZE), errno, EINVAL);
+	fprintf(stderr,"%d %d %d\n", read(fd,buf_ptr,CYLINDER_SIZE), errno, EINVAL);
+	*/
+	
+	time3 = get_time();
+	for(i = 0; i < SEEK_TIME; i++){
+		time1 = get_time();
+		assert(lseek64(fd,SEEK_SIZE,SEEK_CUR) > 0);
+		assert(read(fd,buf_ptr,SECTOR_SIZE) == SECTOR_SIZE);
+		time2 = get_time();
+		time[i] = time2-time1;
+	}
+	time4 = get_time();
 
-	seek_time = time2-time1;
+	total_time = time4 - time3;
+	seek_time = total_time/SEEK_TIME;
 
-	fprintf(stdout,"%f\n", seek_time);
+
+	for(i=0; i < SEEK_TIME; i++){
+		fprintf(stdout,"%f\n",time[i]);
+	}
+	fprintf(stdout,"average: %f\n", seek_time);
 
 	close(fd);
 	free(buf_ptr);
