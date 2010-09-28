7345cf983791fa91a32d175ec020b46b25a5cc4a
yangsuli <yangsuli@ee3314ba-06e1-11df-b5c4-2942a6c37bad>
2010-09-28 03:45:00 +0000

    move stuff to hw1.h

--- hw1/context.c
+++ hw1/context.c
@@ -1,25 +1,10 @@
 #include <sched.h>
-#include <sys/time.h>
 #include <assert.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "hw1.h"
 
 #define REPEAT_SIZE 500
-
-double time_offset; //in seconds
-
-double get_time_offset(){
-	struct timeval t;
-	int rc = gettimeofday(&t,NULL);
-	assert(rc==0);
-	time_offset = t.tv_sec;
-}
-
-double get_time(){
-	struct timeval t;
-	int rc = gettimeofday(&t,NULL);
-	assert(rc == 0);
-	return 1e3*(t.tv_sec - time_offset) + t.tv_usec/1e3;
-}
 
 int main(){
 	struct sched_param param;
